46a60a46301ce10475526bf30ceccf83a078e38e
Tomas5412 <tom.asar5412@gmail.com>
2025-07-25 04:09:21 -0300

    A second button has hit the app

--- src/app.py
+++ src/app.py
@@ -10,6 +10,7 @@ from os import path
 THEME_FILE = "forest-dark.tcl"
 THEME_NAME = "forest-dark"
 
+
 def startChatnalisis():
     filename = askopenfilename()
     message = analizeChat(filename)
@@ -22,21 +23,37 @@ def startChatnalisis():
         print(e)
 
 
-
+root = tk.Tk()
+# Import the tcl file
+root.tk.call('source', f'appTheme/{THEME_FILE}')
+# Set the theme with the theme_use method
+ttk.Style().theme_use(THEME_NAME)
 
 
 # # Tk().withdraw() # we don't want a full GUI, so keep the root window from appearing
 # filename = askopenfilename() # show an "Open" dialog box and return the path to the selected file
 # print(filename)
 
-if __name__ == "__main__":
-    # print("File under construction! Use Chatnalizer for a primitve chatnalisis.")
-    root = tk.Tk()
-    # Import the tcl file
-    root.tk.call('source', f'appTheme/{THEME_FILE}')
-    # Set the theme with the theme_use method
-    ttk.Style().theme_use(THEME_NAME)
-    # A themed (ttk) button
-    button = ttk.Button(root, text="Start Chatnalisis", command=startChatnalisis,  style="Accent.TButton")
-    button.pack(pady=20)
-    root.mainloop()
+# True = English. False = Spanish
+language = tk.BooleanVar()
+language.set(True)
+languageText = tk.StringVar(master=root,value="Change language")
+chanalisisText = tk.StringVar(master=root,value="Start Chatnalisis")
+
+def changeLanguage():
+    language.set(not language.get())
+    if language.get():
+        languageText.set("Change language")
+        chanalisisText.set("Start Chatnalisis")
+    else:
+        languageText.set("Cambiar lenguaje")
+        chanalisisText.set("Empezar Chatnálisis")
+
+# print("File under construction! Use Chatnalizer for a primitve chatnalisis.")
+# A themed (ttk) button
+button = ttk.Button(root, textvariable=chanalisisText, command=startChatnalisis,  style="Accent.TButton")
+button.pack(padx=40,pady=40)
+
+languageutton = ttk.Button(root, textvariable=languageText, command=changeLanguage)
+languageutton.pack(padx=40,pady=40)
+root.mainloop()
